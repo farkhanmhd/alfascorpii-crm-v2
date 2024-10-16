9ca4244bbe8e2fbd3f514d21d205db2ab308230a
farkhanmhd <farkhanmuhammadv3@gmail.com>
2024-10-16 14:51:04 +0700

    added search functionality

--- src/components/fragments/searchDialog/index.tsx
+++ src/components/fragments/searchDialog/index.tsx
@@ -1,17 +1,8 @@
 'use client';
 
 import React from 'react';
-
-import { useAtom } from 'jotai';
-
-import {
-  Calendar,
-  Calculator,
-  CreditCard,
-  Settings,
-  Smile,
-  User,
-} from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import useSearchDialog from '@/hooks/useSearchDialog';
 
 import {
   CommandDialog,
@@ -20,51 +11,54 @@ import {
   CommandEmpty,
   CommandGroup,
   CommandItem,
-  CommandShortcut,
   CommandSeparator,
 } from '@/components/ui/command';
 
-import { searchDialogAtom } from '@/store';
+import { NavItems } from '../sidenav/constants';
 
 const SearchDialog = () => {
-  const [searchDialog, setSearchDialog] = useAtom(searchDialogAtom);
+  const router = useRouter();
+  const { searchDialog, setSearchDialog } = useSearchDialog();
+  const handleSelected = (href: string) => {
+    setSearchDialog(false);
+    router.push(href);
+  };
   return (
     <CommandDialog open={searchDialog} onOpenChange={setSearchDialog}>
       <CommandInput placeholder="Type a command or search..." />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Suggestions">
-          <CommandItem>
-            <Calendar className="mr-2 h-4 w-4" />
-            <span>Calendar</span>
-          </CommandItem>
-          <CommandItem>
-            <Smile className="mr-2 h-4 w-4" />
-            <span>Search Emoji</span>
-          </CommandItem>
-          <CommandItem>
-            <Calculator className="mr-2 h-4 w-4" />
-            <span>Calculator</span>
-          </CommandItem>
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup heading="Settings">
-          <CommandItem>
-            <User className="mr-2 h-4 w-4" />
-            <span>Profile</span>
-            <CommandShortcut>⌘P</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <CreditCard className="mr-2 h-4 w-4" />
-            <span>Billing</span>
-            <CommandShortcut>⌘B</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <Settings className="mr-2 h-4 w-4" />
-            <span>Settings</span>
-            <CommandShortcut>⌘S</CommandShortcut>
-          </CommandItem>
-        </CommandGroup>
+        {NavItems.map((item) =>
+          item.isParent ? (
+            <>
+              <CommandGroup key={item.title} heading={item.title}>
+                {item.childrens?.map((child) => (
+                  <CommandItem
+                    key={child.title}
+                    onSelect={() => handleSelected(child.href ?? '')}
+                  >
+                    <span className="mr-2 h-4 w-4">{child.icon}</span>
+                    <span>{child.title}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+              <CommandSeparator />
+            </>
+          ) : (
+            <>
+              <CommandGroup key={item.title} heading={item.title}>
+                <CommandItem
+                  key={item.title}
+                  onSelect={() => handleSelected(item.href ?? '')}
+                >
+                  <span className="mr-2 h-4 w-4">{item.icon}</span>
+                  <span>{item.title}</span>
+                </CommandItem>
+              </CommandGroup>
+              <CommandSeparator />
+            </>
+          )
+        )}
       </CommandList>
     </CommandDialog>
   );
